c6170746760a709688c0ea5a9f72a524a9228123
vaibhav537 <vaibhavmali537@gmail.com>
2025-05-16 01:12:48 +0530

    Refactor code structure for improved readability and maintainability

--- classifyai/src/app/api/attendance/create-token/route.ts
+++ classifyai/src/app/api/attendance/create-token/route.ts
@@ -1,3 +1,37 @@
-export function POST(request: Request) {
-  return new Response("Hello, World!");
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+import QRCode from "qrcode";
+
+export async function POST(request: Request) {
+  try {
+    const { subject, professorID } = await request.json();
+    if (!subject || !professorID) {
+      return NextResponse.json(
+        { message: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+    const attendanceToken = uuidv4();
+
+    const payload = JSON.stringify({
+      token: attendanceToken,
+      subject,
+      issuedAt: Date.now(),
+      professorID,
+    });
+
+    const qrCodeDataUrl = await QRCode.toDataURL(payload);
+
+    //? TODO : Save the attendance token and QR code to the database
+    return NextResponse.json(
+      { qrCodeDataUrl, attendanceToken },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error generating QR code:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
